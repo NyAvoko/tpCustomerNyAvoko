84808e72aca31a6289d674ebdfbd8c50c1cd5185
Avoko <Avoko@LABOOI-146.labooi.mg>
2024-03-20 11:38:49 +0300

    Etat Initial

---
+++ src/main/java/com/nyavoko/tp1denyavoko/resources/JakartaEE10Resource.java
@@ -0,0 +1,20 @@
+package com.nyavoko.tp1denyavoko.resources;
+
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.core.Response;
+
+/**
+ *
+ * @author 
+ */
+@Path("jakartaee10")
+public class JakartaEE10Resource {
+    
+    @GET
+    public Response ping(){
+        return Response
+                .ok("ping Jakarta EE ok avec Avoko")
+                .build();
+    }
+}
