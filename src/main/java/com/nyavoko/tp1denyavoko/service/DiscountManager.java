14a9285da4111cbf892c132c2d11cf1ebd68c10b
Avoko <Avoko@LABOOI-146.labooi.mg>
2024-03-23 13:05:44 +0300

    amelioration sur discount

--- src/main/java/com/nyavoko/tp1denyavoko/service/DiscountManager.java
+++ src/main/java/com/nyavoko/tp1denyavoko/service/DiscountManager.java
@@ -5,6 +5,7 @@
 package com.nyavoko.tp1denyavoko.service;
 
 import com.nyavoko.tp1denyavoko.entity.Customer;
+import com.nyavoko.tp1denyavoko.entity.Discount;
 import jakarta.enterprise.context.RequestScoped;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
@@ -15,30 +16,24 @@ import java.util.List;
 /**
  *
  * @author Avoko
- *  * Gère la persistance des Customers.
  */
 @RequestScoped
-public class CustomerManager {
+public class DiscountManager {
 
-    @PersistenceContext(unitName = "customerPU")
+    @PersistenceContext(unitName = "discountPU")
     private EntityManager em;
 
-    public List<Customer> getAllCustomers() {
-        Query query = em.createNamedQuery("Customer.findAll");
+    public List<Discount> getAllDiscounts() {
+        Query query = em.createNamedQuery("Discount.findAll");
         return query.getResultList();
     }
 
-    @Transactional
-    public Customer update(Customer customer) {
+     @Transactional
+    public Discount update(Discount discount) {
         return null;
     }
-
-    @Transactional
-    public void persist(Customer customer) {
-        em.persist(customer);
-    }
-
-    public Customer findById(int idCustomer) {
-    return em.find(Customer.class, idCustomer);
+    
+    public Discount findById(String code) {
+        return em.find(Discount.class, code);
     }
 }
